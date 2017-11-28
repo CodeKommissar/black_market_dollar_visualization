0128eca40ba45fe144eb9e3a777d4e14827905c4
luisrodriguez <codekommissar@gmail.com>
2017-11-27 21:40:17 -0400

    Add Todos

--- app.js
+++ app.js
@@ -20,3 +20,11 @@ app.get("/", function(req, res){
 app.listen(port, function(){
   console.log("Server is listening on: " + port);
 });
+
+// TODO:
+// 1. Separate visualization script to index.ejs to it's own file
+
+// Due to how Heroku works:
+// 2. Add a line to call scrapeDolarToday() every time the server starts again
+// 3. In the visualization script get the last line of .tsv file and... 
+// check if we're adding a new piece of data
