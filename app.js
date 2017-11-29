14cf42348b9e3d760dd914d73a990d98c4e47685
luisrodriguez <codekommissar@gmail.com>
2017-11-28 21:08:00 -0400

    separated d3 code into its own file

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // Every day scrape http://dolartoday.com
+//scrapeDolarToday();
 setInterval(scrapeDolarToday, 1000*60*60*24)
 
 // Render our index.html page in the "/" route
@@ -21,10 +22,7 @@ app.listen(port, function(){
   console.log("Server is listening on: " + port);
 });
 
-// TODO:
-// 1. Separate visualization script to index.ejs to it's own file
-
 // Due to how Heroku works:
-// 2. Add a line to call scrapeDolarToday() every time the server starts again
-// 3. In the visualization script get the last line of .tsv file and... 
+// 1. Add a line to call scrapeDolarToday() every time the server starts again
+// 2. In the visualization script get the last line of .tsv file and... 
 // check if we're adding a new piece of data
